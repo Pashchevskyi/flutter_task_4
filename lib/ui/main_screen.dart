4c2fbd38080151a611529154e0d473df323b3f3c
Pavlo Pashchevskyi <googalltooth4@gmail.com>
2022-12-30 14:54:00 +0200

    Users are now printed and added via AlertDialog instead of hardcoding and printing into console

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -15,61 +15,129 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
+    BlocProvider.of<UsersBloc>(context).add(LoadUsersEvent());
     // BlocBuilder, BlocConsumer
     return BlocListener<UsersBloc, UsersState>(
       listener: (BuildContext context, state) {
-        if (state is UsersLoadedState) {
-          print(state.users);
-        }
       },
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-              onPressed: _addTestValues,
-              child: const Text('Add test users'),
+              onPressed: _showAddUserDialog,
+              child: const Text('Add User'),
             ),
             ElevatedButton(
-              onPressed: _printUsers,
+              onPressed: () {
+                _showUsersDialog();
+                },
               child: const Text('Print users'),
             ),
+
           ],
         ),
       ),
     );
   }
 
-  void _addTestValues() {
-    final bloc = BlocProvider.of<UsersBloc>(context);
-    bloc.add(
-      AddUserEvent(
-        User(
-          name: 'Alex',
-          age: 22,
-        ),
-      ),
-    );
-    bloc.add(
-      AddUserEvent(
-        User(
-          name: 'Ben',
-          age: 33,
-        ),
-      ),
-    );
-    bloc.add(
-      AddUserEvent(
-        User(
-          name: 'Carl',
-          age: 44,
-        ),
-      ),
+  Future<void> _showAddUserDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        var nameController = TextEditingController();
+        var ageController = TextEditingController();
+        return AlertDialog(
+          title: Text('Add user'),
+          content: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(labelText: 'Name'),
+              ),
+              TextFormField(
+                controller: ageController,
+                decoration: InputDecoration(labelText: 'Age'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                // Send them to your email maybe?
+                _addUser(nameController.text, int.parse(ageController.text));
+                Navigator.pop(context);
+              },
+              child: Text('Send'),
+            ),
+          ],
+        );
+      },
     );
   }
 
-  void _printUsers() {
+  Future<void> _showUsersDialog() async {
+    return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return _outputUsersInTable();
+        });
+  }
+
+  Widget _outputUsersInTable() {
+    List<Text> tableCells = [];
+    Table table = Table(children: [],);
+    UsersState state = BlocProvider.of<UsersBloc>(context).state;
+    if (state is UsersLoadedState) {
+      table.children.add(
+        TableRow(children: [
+          Text("ID"),
+          Text("Name"),
+          Text("Age"),
+        ]),
+      );
+      state.users.sort((User a, User b) {return a.name.compareTo(b.name);});
+      for (User user in state.users) {
+        tableCells.add(Text("${user.id}"));
+        tableCells.add(Text(user.name));
+        tableCells.add(Text("${user.age}"));
+      }
+      table.children.add(
+        TableRow(children: tableCells)
+      );
+    }
+    return AlertDialog(
+      title: const Text("Users"),
+      content: SingleChildScrollView(
+        child: table,
+      ),
+      actions: [
+        TextButton(
+          child: const Text('OK'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+
+  void _addUser(String strName, int iAge) {
     final bloc = BlocProvider.of<UsersBloc>(context);
+    bloc.add(
+      AddUserEvent(
+        User(
+          name: strName,
+          age: iAge,
+        ),
+      ),
+    );
     bloc.add(LoadUsersEvent());
   }
 }
